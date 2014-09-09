fdca8c000374288ea07f8cff74aebdfd08e80ccf
Miguel J Velazquez <migueljosuevelazquez@gmail.com>
2014-09-08 20:29:47 -0500

    json parse first round

--- encode-and-use.php
+++ encode-and-use.php
@@ -1,20 +1,44 @@
-
-
 <?php
+//array de frutas
 	$fruits = ['apple','pear','banana','watermelon','mango'];
 	$jsonFruits =  json_encode($fruits);
+//associative array de fruta y color
+	$fruitColor= array(
+		"apple"=>"red",
+		"orange"=>"orange",
+		"banana"=>"yellow",
+		"mango"=>"orange"
+		);
+	$fruitColor = json_encode($fruitColor);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>A Document</title>
+	<title>Generando y Usando JSON</title>
 </head>
 <body>
 	
 	<script>
-			var jsonfrutas = (<?php echo $jsonFruits; ?>);
-			alert(jsonfrutas);
+//imprimiendo como array
+		var frutasArray = <?php echo $jsonFruits; ?>;
+	//accesando solo uno
+		document.write(frutasArray[0]);
+			document.write('<br />');
+	//imprimiendo todo el array
+		document.write(frutasArray);
+				document.write('<br />');
+
+//convert string to object
+		var frutasObject = JSON.parse('<?php echo $fruitColor; ?>');
+	//accessando solo uno 
+		document.write(frutasObject.apple);
+				document.write('<br />');
+		document.write(frutasObject['apple']);
+				document.write('<br />');
+
+	//imprimiendo todo el objeto
 	</script>
 </body>
 
